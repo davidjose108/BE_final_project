577f01357551c5da5c3324296fd9c4fc3e8ea864
David Garrido <115107126+davidjose108@users.noreply.github.com>
2023-11-22 20:46:54 +0100

    Add files via upload

--- cardentry.py
+++ cardentry.py
@@ -1,144 +1,15 @@
-from main import collection
+import requests
 
-# Define a tarot card document
-major_arcana_cards = [
-    {
-        "number": 1,
-        "name": "The Magician",
-        "description": "Manifestation, power, resourcefulness.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 2,
-        "name": "The High Priestess",
-        "description": "Intuition, mystery, the subconscious.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 3,
-        "name": "The Empress",
-        "description": "Nurturing, fertility, abundance.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 4,
-        "name": "The Emperor",
-        "description": "Authority, structure, leadership.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 5,
-        "name": "The Hierophant",
-        "description": "Tradition, spiritual guidance, education.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 6,
-        "name": "The Lovers",
-        "description": "Love, relationships, choices.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 7,
-        "name": "The Chariot",
-        "description": "Control, determination, victory.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 8,
-        "name": "Strength",
-        "description": "Inner strength, courage, patience.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 9,
-        "name": "The Hermit",
-        "description": "Solitude, introspection, wisdom.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 10,
-        "name": "Wheel of Fortune",
-        "description": "Cycles, destiny, change.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 11,
-        "name": "Justice",
-        "description": "Fairness, balance, legal matters.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 12,
-        "name": "The Hanged Man",
-        "description": "Surrender, letting go, sacrifice.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 13,
-        "name": "Death",
-        "description": "Transformation, change, renewal.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 14,
-        "name": "Temperance",
-        "description": "Balance, moderation, harmony.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 15,
-        "name": "The Devil",
-        "description": "Materialism, bondage, temptation.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 16,
-        "name": "The Tower",
-        "description": "Destruction, upheaval, revelation.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 17,
-        "name": "The Star",
-        "description": "Hope, inspiration, spirituality.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 18,
-        "name": "The Moon",
-        "description": "Illusion, intuition, subconscious.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 19,
-        "name": "The Sun",
-        "description": "Joy, success, vitality.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 20,
-        "name": "Judgment",
-        "description": "Reckoning, transformation, rebirth.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 21,
-        "name": "The World",
-        "description": "Completion, accomplishment, fulfillment.",
-        "is_birth_card": False,
-    },
-    {
-        "number": 0,
-        "name": "The Fool",
-        "description": "New beginnings, spontaneity, unpredictability.",
-        "is_birth_card": False,
-    },
-]
+url = "http://127.0.0.1:5000/createcard"
 
+tarot_card_data = {
+    "number": 23,
+    "name": "The Queen",
+    "description": "Authentic, brave, powerful",
+    "is_birth_card": False,
+}
 
-# Insert the list of major arcana tarot cards into the collection
-result = collection.insert_many(major_arcana_cards)
+response = requests.post(url, json=tarot_card_data)
 
-# Print the inserted documents' IDs
-print(f"Inserted document IDs: {result.inserted_ids}")
+print(response.status_code)
+print(response.json())
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from flask import Flask, jsonify, request
 
 from database import client, collection, db
@@ -13,42 +11,14 @@ def root():
     return {"message": "Welcome to your Tarot Journal"}
 
 
-@app.route("/pick", methods=["GET"])
-def get_random_tarot_card():
-    # Fetch a random tarot card from the MongoDB collection
-    random_card = collection.aggregate([{"$sample": {"size": 1}}])
-
-    # Check if any card is returned
-    if random_card.alive:
-        card = list(random_card)[0]  # Extract the card from the cursor
-
-        # Remove the _id field from the card
-        card.pop("_id", None)
-
-        # Return the tarot card as a JSON response
-        return jsonify({"tarot_card": card})
-    else:
-        return jsonify({"error": "No tarot cards found"}), 404
-
-
-@app.route("/allcards", methods=["GET"])
-def get_all_tarot_cards():
-    # Fetch all tarot cards from the MongoDB collection
-    all_cards = list(collection.find({}, {"_id": False}))
-
-    return jsonify({"tarot_cards": all_cards})
-
-
-@app.route("/create_tarot_card", methods=["POST"])
-def create_tarot_card():
+# CREATE a card
+@app.route("/createcard", methods=["POST"])
+def createcard():
     try:
         tarot_card_data = TarotCard(**request.json)
     except Exception as e:
         return jsonify({"error": str(e)}), 400
-
-    # Process tarot_card_data and save it to the database
-    # For example, you can use tarot_card_data.name, tarot_card_data.description, tarot_card_data.number, and tarot_card_data.is_birth_card
-    result = collection.insert_one(tarot_card_data.dict())
+    result = collection.insert_one(tarot_card_data.model_dump())
 
     return jsonify(
         {
@@ -58,63 +28,77 @@ def create_tarot_card():
     )
 
 
+# GET a card from the number
+@app.route("/card/<int:card_number>", methods=["GET"])
+def get_card_by_number(card_number):
+    tarot_card = collection.find_one({"number": card_number})
+    tarot_card.pop("_id", None)
+    return jsonify({"tarot_card": tarot_card})
+
+
+# GET a random card
+@app.route("/pick", methods=["GET"])
+def get_random_tarot_card():
+    random_card = collection.aggregate([{"$sample": {"size": 1}}])
+    if random_card.alive:
+        card = list(random_card)[0]
+        card.pop("_id", None)
+        return jsonify({"tarot_card": card})
+    else:
+        return jsonify({"error": "No tarot cards found"}), 404
+
+
+# GET the list of all cards (documents)
+@app.route("/allcards", methods=["GET"])
+def get_all_tarot_cards():
+    all_cards = list(collection.find({}, {"_id": False}))
+    return jsonify({"tarot_cards": all_cards})
+
+
+# DELETE a card
 @app.route("/delete_tarot_card", methods=["DELETE"])
 def delete_tarot_card():
     card_name = request.args.get("name")
-
-    if not card_name:
-        return jsonify({"error": "Name parameter is required for deletion"}), 400
-
     result = collection.delete_one({"name": card_name})
-
-    if result.deleted_count == 1:
-        return jsonify({"message": f"Tarot card {card_name} deleted successfully"})
-    else:
-        return jsonify({"error": f"Tarot card {card_name} not found"}), 404
+    return jsonify({"message": f"Tarot card {card_name} deleted successfully"})
 
 
-@app.route("/set_birth_card/<name>", methods=["PUT"])
+@app.route("/my_birth_card/<name>", methods=["PUT"])
 def set_birth_card(name):
-    print(f"Trying to update is_birth_card for {name}")
+    # We want to first check the value of the card to know if it is true or false
+    tarot_card = collection.find_one({"name": name})
 
-    # Update the 'is_birth_card' field to True for the given tarot card name
-    result = collection.update_one({"name": name}, {"$set": {"is_birth_card": True}})
-    return jsonify({"message": f"Tarot card {name} updated as birth card"})
-    # print(f"Modified count: {result.modified_count}")
+    if tarot_card:
+        current_value = tarot_card.get("is_birth_card", False)
 
-    # if result.modified_count == 1:
-    #     return jsonify({"message": f"Tarot card {name} updated as birth card"})
-    # else:
-    #     return jsonify({"error": f"Failed to update tarot card {name}"}), 500
+        # Update the field to the opposite
+        result = collection.update_one(
+            {"name": name}, {"$set": {"is_birth_card": not current_value}}
+        )
+        return jsonify({"message": f"Tarot card {name} updated"})
+    else:
+        return jsonify({"error": f"Failed to update tarot card {name}"}), 500
 
 
-@app.route("/get_birth_card", methods=["POST"])
+@app.route("/my_birth_card", methods=["POST"])
 def get_birth_card():
     try:
         # Get the birthdate from the request JSON
         birthdate = request.json.get("birthdate")
-
-        # Validate the format of the birthdate (assuming YYYY-MM-DD)
-        datetime.strptime(birthdate, "%Y-%m-%d")
-
         # Calculate the sum of individual digits in the birthdate
         digit_sum = sum(int(digit) for digit in birthdate if digit.isdigit())
-
+        print(digit_sum)
         # Map the digit sum to a number between 0 and 22
         birth_card_number = digit_sum % 23
-
+        print(birth_card_number)
         # Retrieve the tarot card corresponding to the birth card number
         tarot_card = collection.find_one({"number": birth_card_number})
 
         if tarot_card:
+            tarot_card["_id"] = str(tarot_card["_id"])
             return jsonify({"tarot_card": tarot_card})
-        else:
-            return jsonify({"error": "Failed to retrieve tarot card"}), 500
-
     except ValueError:
         return jsonify({"error": "Invalid date format"}), 400
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == "__main__":
