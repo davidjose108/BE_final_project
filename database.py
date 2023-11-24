665430e8d67ee0c0bdbb32286e1ed192260276d7
David Garrido <115107126+davidjose108@users.noreply.github.com>
2023-11-24 21:59:15 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 connection_string = (
-    "mongodb+srv://davidjose108:cN9guYoIbJgnxyFb@cluster0.at2q4em.mongodb.net/"
+    "mongodb+srv://davidjose108:<Password>b@cluster0.at2q4em.mongodb.net/"
 )
 
 client = MongoClient(connection_string)
